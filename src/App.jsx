cc9e5e1d0d6df81c65d7bba283793d563416ad80
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-04-30 04:37:06 +0100

    Adding header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Footer from './component/Footer'
-import Main from './component/Main'
+import React from 'react';
+import Footer from './component/Footer';
+import Main from './component/Main';
+import Header from './component/Header';
 
 const App = () => {
   return (
     <div className='app'>
+      <Header />
       <Main />
       <Footer />
     </div>
---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <h1>age calculator app</h1>
+        <p>Insert your date of birth to know how long you have lived</p>
+    </header>
+  )
+}
+
+export default Header
