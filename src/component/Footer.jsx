6ab0775d5bb6844743463704e4fd998f3c953b37
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-04-30 11:53:08 +0100

    Updating footerinfo

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel='noreferrer' target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://emmanuel-obiora.github.io/portfolio-about-me-/src" rel='noreferrer' target='_blank'>Your Name Here</a>.
+        Coded by <a href="https://emmanuel-obiora.github.io/portfolio-about-me-/src" rel='noreferrer' target='_blank'>Obiora Emmanuel</a>.
     </footer>
   )
 }
